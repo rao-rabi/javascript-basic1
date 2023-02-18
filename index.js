613882ddedd7ef361f655b3dda6fb28ca8b730ef
Rao-Rabi <rabirao1039@gmail.com>
2023-02-18 16:10:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+//question#1
+
+let name='Umair Azmat';
+let admin = name;
+alert(admin);
+
+//question#2
+
+let earth="planet;"
+
+//question#3
+
+let present_visitor = "Rabi";
+
+//question#4
+
+let favDrink = "Pepsi";
+
+//question#5
+
+let favNum = 5;
+
+//question#6
+
+function myFavs() {
+    document.write("hello everyone");
+}
+myFavs();
+
+//question#7
+
+let person = {
+    name : "ali",
+    age : 20
+}
+
+//question#8
+
+let num1 = 2;
+let num2 = 4;
+let sum = num1 + num2;
+console.log(sum);
+
+//question#9
+
+let num3 = 8;
+let num4 = 4;
+let a = num3/num4;
+console.log("result of firstprogram",a);
+
+//question#10
+
+let num5 = 8;
+let num6 = 10;
+let b = (num5 + num6)/5;
+console.log("result of firstprogram",b);
+
+//question#11
+
+let F= 50;
+let C= (F-32)*(5/9);
+console.log("Temprature in celcius is: ", C);
+
+//question#12
+
+let num7 = parseInt(prompt("Enter 1st number:"));
+let num8 = parseInt(prompt("Enter 2nd number:"));
+alert(num7 + num8);
+
+//question#13
+
+let num9 = parseInt(prompt("Enter 1st number:"));
+let num10 = parseInt(prompt("Enter 2nd number:"));
+alert(num9 - num10);
+
+//question#14
+
+let num11 = parseInt(prompt("Enter 1st number:"));
+let num12 = parseInt(prompt("Enter 2nd number:"));
+alert(num11 * num12);
+
+//question#15
+
+let num13 = parseInt(prompt("Enter 1st number:"));
+let num14 = parseInt(prompt("Enter 2nd number:"));
+alert(num13 / num14);
+
+//#question#16
+
+let num15 = parseInt(prompt("Enter 1st number:"));
+let num16 = parseInt(prompt("Enter 2nd number:"));
+alert(num15 % num16);
+
+
+
+
+
+
+
